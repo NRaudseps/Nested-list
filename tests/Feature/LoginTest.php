d98610bdb92f044ed8d3f096b76f0322dca69a94
nraudseps <nraudseps@gmail.com>
2020-11-26 13:53:38 +0200

    added test functionality

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class LoginTest extends TestCase
+{
+    /** @test */
+    public function assert_true()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
